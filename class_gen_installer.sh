7e5ce48f204105ebc8ed7ed36268d430543e9948
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2024-06-16 22:34:20 +0200

    Fix: Added the missing <sstream> header Fix: Fixed the spaces in the bash script before =

--- class_gen_installer.sh
+++ class_gen_installer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-REPO_URL = "https://github.com/AlRodriguezGar14/generate_class.git class_generator"
-EXECUTABLE = "class_generator"
-DIR = "/tmp/class_generator_alrodriguezgar14"
+REPO_URL="https://github.com/AlRodriguezGar14/generate_class.git"
+EXECUTABLE="class_generator"
+DIR="/tmp/class_generator_alrodriguezgar14"
 
 git clone $REPO_URL $DIR
 cd DIR
