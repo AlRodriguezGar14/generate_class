c9426d8f38bb2cf3340186e926dc485178558c26
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2024-06-16 22:43:27 +0200

    Fix: Fixed typho in the script, update the executable name

--- class_gen_installer.sh
+++ class_gen_installer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 REPO_URL="https://github.com/AlRodriguezGar14/generate_class.git"
-EXECUTABLE="class_generator"
+EXECUTABLE="generate_class"
 DIR="/tmp/class_generator_alrodriguezgar14"
 
 git clone $REPO_URL $DIR
-cd DIR
+cd $DIR
 make
 
 mv $EXECUTABLE /usr/local/bin
